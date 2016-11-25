b56d184c36ac8fbf68d99c3a5c54083ff7f30cc0
toanvn <vantoan23@gmail.com>
2016-11-25 17:24:07 +0700

    added google map

--- src/models/GoogleMap.js
+++ src/models/GoogleMap.js
@@ -3,14 +3,42 @@ import MapStyle from '../less/index.less'
 
 GMap.KEY = 'AIzaSyDXUMCX-pZb6F4YHsL4gu34SUZ4say46B8';
 GMap.LIBRARIES = ['geometry', 'places'];
-
-const map_options = {
-  zoom: 12,
-  center: {lat: 16.073076, lng: 108.224963}
-};
-
-const map_contain = document.getElementById('gmapWrapper');
-
 GMap.load(function(google) {
-    var map = new google.maps.Map(map_contain, map_options);
+      initMap();
 });
+
+var poly;
+var map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('gmapWrapper'), {
+    zoom: 7,
+    center: {lat: 41.879, lng: -87.624}  // Center the map on Chicago, USA.
+  });
+
+  poly = new google.maps.Polyline({
+    strokeColor: '#000000',
+    strokeOpacity: 1.0,
+    strokeWeight: 3
+  });
+  poly.setMap(map);
+
+  // Add a listener for the click event
+  map.addListener('click', addLatLng);
+}
+
+// Handles click events on a map, and adds a new point to the Polyline.
+function addLatLng(event) {
+  var path = poly.getPath();
+
+  // Because path is an MVCArray, we can simply append a new coordinate
+  // and it will automatically appear.
+  path.push(event.latLng);
+
+  // Add a new marker at the new plotted point on the polyline.
+  var marker = new google.maps.Marker({
+    position: event.latLng,
+    title: '#' + path.getLength(),
+    map: map
+  });
+}
